436449c9d35eded4928b80a1c8a7f008628b499e
Cheng <git@zcbenz.com>
2024-09-15 15:57:53 +0900

    Collect memories of tensors in worker

--- src/worker.ts
+++ src/worker.ts
@@ -1,18 +1,21 @@
 import {parentPort, workerData} from 'node:worker_threads';
+import {core as mx} from '@frost-beta/mlx';
 import {Clip} from '@frost-beta/clip';
 
 import type {BatchMessage, BatchResponse} from './model.js';
 
 const clip = new Clip(workerData.modelDir);
 
-parentPort.on('message', async ({id, labels, images}: BatchMessage) => {
+parentPort.on('message', ({id, labels, images}: BatchMessage) => {
   if (id == 0)
     process.exit(0);
-  const output = await clip.computeEmbeddings({labels, images});
-  const response: BatchResponse = {id};
-  if (labels)
-    response.labelEmbeddings = output.labelEmbeddings.tolist() as number[][];
-  if (images)
-    response.imageEmbeddings = output.imageEmbeddings.tolist() as number[][];
-  parentPort.postMessage(response);
+  mx.tidy(() => {
+    const output = clip.computeEmbeddings({labels, images});
+    const response: BatchResponse = {id};
+    if (labels)
+      response.labelEmbeddings = output.labelEmbeddings.tolist() as number[][];
+    if (images)
+      response.imageEmbeddings = output.imageEmbeddings.tolist() as number[][];
+    parentPort.postMessage(response);
+  });
 });
